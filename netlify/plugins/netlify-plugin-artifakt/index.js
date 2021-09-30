08195c5668338b6abfd38d9c7659e3058273e7c5
Sylvano <stm@esokia-webagency.com>
2021-09-30 14:42:52 +0300

    add await before axios

--- netlify/plugins/netlify-plugin-artifakt/index.js
+++ netlify/plugins/netlify-plugin-artifakt/index.js
@@ -3,7 +3,7 @@ const axios = require("axios")
 module.exports = {
     async onSuccess () {
         console.log('Axios begin')
-        axios.get('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
+        await axios.get('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
             console.log(response.data)
         }).catch(error => {
             console.log(error)
