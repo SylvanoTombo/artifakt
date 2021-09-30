f15d9b3759758b82d86cc8b8d0084a212e20ebff
Sylvano <stm@esokia-webagency.com>
2021-09-30 14:41:07 +0300

    delete await before axios

--- netlify/plugins/netlify-plugin-artifakt/index.js
+++ netlify/plugins/netlify-plugin-artifakt/index.js
@@ -3,8 +3,8 @@ const axios = require("axios")
 module.exports = {
     async onSuccess () {
         console.log('Axios begin')
-        await axios.get('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
-            console.log(response)
+        axios.get('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
+            console.log(response.data)
         }).catch(error => {
             console.log(error)
         })
